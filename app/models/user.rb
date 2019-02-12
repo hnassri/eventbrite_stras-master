7ac442bcebd31d76ce0e094eaa15056d0bc2a4c5
hnassri <nassrihamdi@gmail.com>
2019-02-12 19:50:15 +0100

    modif d'une erreur

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   # after_create :welcome_send
-   after_update :redirect
   has_many :events
   has_many :attendances
   has_many :events, through: :attendances
@@ -13,8 +12,5 @@ class User < ApplicationRecord
     UserMailer.welcome_email(self).deliver_now
   end
 
-  def redirect
-    user_path(current_user.id)
-  end
 
 end
