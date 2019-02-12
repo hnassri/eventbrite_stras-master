b36fa53924b1e86d0e3debb4c9ab5783d223362f
hnassri <nassrihamdi@gmail.com>
2019-02-12 19:40:20 +0100

    lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  after_create :welcome_send
-  after_update :redirect
+  # after_create :welcome_send
+   after_update :redirect
   has_many :events
   has_many :attendances
   has_many :events, through: :attendances
