9bd998e5be37187ff2d357c2e82c00f4e7efa2dc
hnassri <nassrihamdi@gmail.com>
2019-02-13 18:56:53 +0100

    lol

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,7 +5,7 @@ class Attendance < ApplicationRecord
   belongs_to :event
 
   def welcome_send
-    EventMailer.welcome_event(Event.find(params[:event_id])).deliver_now
+    EventMailer.welcome_event(Event.find(event_id)).deliver_now
   end
 
 end
