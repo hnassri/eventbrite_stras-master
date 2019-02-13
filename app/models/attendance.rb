7af7823618549ed842837549ee36ab0b1ab11fc8
hnassri <nassrihamdi@gmail.com>
2019-02-13 18:38:27 +0100

    ajout mailer participant

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,11 +1,11 @@
 class Attendance < ApplicationRecord
-  # after_create :welcome_send
+  after_create :welcome_send
 
   belongs_to :user
   belongs_to :event
 
   def welcome_send
-    EventMailer.welcome_event(Event.find(event_id)).deliver_now
+    EventMailer.welcome_event(Event.find(params[:event_id])).deliver_now
   end
 
 end
